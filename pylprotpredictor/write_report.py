b79c326bdcaacfb7a8ca76d3dc366c5f95163524
Bérénice Batut <bebatut@gmail.com>
2018-02-15 11:38:04 +0100

    Rewrite and add test to the write_report script

--- pylprotpredictor/write_report.py
+++ pylprotpredictor/write_report.py
@@ -1,38 +1,47 @@
 import pandas as pd
+
 from snakemake.utils import report
 
 
 def extract_row_number(csv_filepath):
-    """
-    Extract row number of a CSV file
+    """Extract row number of a CSV file
+
+    :param csv_filepath: path to a CSV file
+
+    :return: an integer corresponding to the number of lines in the CSV file
     """
     df = pd.read_csv(csv_filepath, index_col=0, header=0)
     return df.shape[0]
 
 
-if __name__ == '__main__':
-    predicted_cds_nb = extract_row_number(
-        str(snakemake.input.predicted_cds_info))
-    tag_ending_cds_nb = extract_row_number(
-        str(snakemake.input.tag_ending_cds))
-    potential_pyl_protein_nb = extract_row_number(
-        str(snakemake.input.potential_pyl_proteins))
-    cons_pot_pyl_seq_nb = extract_row_number(
-        str(snakemake.input.conserved_potential_pyl_sequences))
+def write_report(pred_cds, tag_ending_cds, pot_pyl_cds, final_cds, report_filepath):
+    """Write HTML report to summarize the full analysis
+
+    :param pred_cds:
+    :param tag_ending_cds:
+    :param pot_pyl_cds:
+    :param final_cds_info:
+    :param report_filepath:
+    """
+    pred_cds_nb = extract_row_number(pred_cds)
+    tag_ending_cds_nb = extract_row_number(tag_ending_cds)
+    pot_pyl_cds_nb = extract_row_number(pot_pyl_cds)
+    final_cds_nb = extract_row_number(final_cds)
 
     report("""
     Prediction of PYL proteins
     ==========================
 
-    {predicted_cds_nb} sequences are predicted as CDS (see Table
-    predicted_cds_info_).
+    {pred_cds_nb} sequences are predicted as CDS (see Table
+    predicted_cds_).
 
-    From these {predicted_cds_nb} prediced CDS, {tag_ending_cds_nb} are ending
+    From these {pred_cds_nb} prediced CDS, {tag_ending_cds_nb} are ending
     with a TAG STOP codon (see Table tag_ending_cds_).
-    {potential_pyl_protein_nb} are predicted as potential PYL sequences (see
-    Table potential_pyl_proteins_).
+    {pot_pyl_cds_nb} are predicted as potential PYL sequences (see
+    Table potential_pyl_cds_).
 
-    After similarity search, {cons_pot_pyl_seq_nb} sequences (of
-    {potential_pyl_protein_nb} predicted PYL sequences) are conserved as
-    potential PYL proteins (see Table conserved_potential_pyl_sequences_).
-    """, str(snakemake.output.report), **snakemake.input)
+    After similarity search, {final_cds_nb} sequences (of
+    {pot_pyl_cds_nb} predicted PYL sequences) are conserved as
+    potential PYL proteins (see Table final_cds_).
+    """, report_filepath, predicted_cds=pred_cds, tag_ending_cds=tag_ending_cds,
+    potential_pyl_cds=pot_pyl_cds, final_cds=final_cds)
